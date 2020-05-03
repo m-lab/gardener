47c4bae29a31829c2efaf085b6c5e612c0e35b25
Gregory Russell <gfr@google.com>
2020-05-02 22:48:33 -0400

    Fix failure update

--- tracker/job_test.go
+++ tracker/job_test.go
@@ -33,3 +33,31 @@ func TestStatusUpdate(t *testing.T) {
 	}
 	t.Log(s.LastUpdate())
 }
+
+func TestStatusFailure(t *testing.T) {
+	s := tracker.NewStatus()
+	s.Update(tracker.Parsing, "init done")
+	if s.History[0].LastUpdate != "init done" {
+		t.Error(s.History[0])
+	}
+	s.UpdateDetail("Still parsing")
+	if s.LastUpdate() != "Still parsing" {
+		t.Error(s.LastUpdate())
+	}
+
+	// Each Update with new State should result in empty LastUpdate field.
+	s.Update(tracker.Deduplicating, "Parsing complete")
+	if s.LastUpdate() != "" {
+		t.Error(s.LastUpdate())
+	}
+
+	s.Update(tracker.Failed, "failed")
+	if len(s.History) != 4 {
+		t.Error("length =", len(s.History))
+	}
+	last := s.LastStateInfo()
+	if last.LastUpdate != "failed" {
+		t.Error(last)
+	}
+	t.Log(s.LastUpdate())
+}
