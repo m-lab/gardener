a51281650eb005e0577f91493e92174d0cae89ae
Stephen Soltesz <stephen.soltesz@gmail.com>
2021-03-09 12:07:01 -0500

    Simplify variables

--- apply-cluster.sh
+++ apply-cluster.sh
@@ -6,14 +6,14 @@
 #
 # Example:
 #
-#   PROJECT=mlab-sandbox CLOUDSDK_CONTAINER_CLUSTER=scraper-cluster ./apply-cluster.sh
+#   PROJECT_ID=mlab-sandbox CLOUDSDK_CONTAINER_CLUSTER=scraper-cluster ./apply-cluster.sh
 
 set -x
 set -e
 set -u
 
-USAGE="PROJECT=<projectid> CLOUDSDK_CONTAINER_CLUSTER=<cluster> $0"
-PROJECT=${PROJECT:?Please provide project id: $USAGE}
+USAGE="PROJECT_ID=<projectid> CLOUDSDK_CONTAINER_CLUSTER=<cluster> $0"
+PROJECT_ID=${PROJECT_ID:?Please provide project id: $USAGE}
 CLUSTER=${CLOUDSDK_CONTAINER_CLUSTER:?Please provide cluster name: $USAGE}
 DATE_SKIP=${DATE_SKIP:-"0"}  # Number of dates to skip between each processed date (for sandbox).
 TASK_FILE_SKIP=${TASK_FILE_SKIP:-"0"}  # Number of files to skip between each processed file (for sandbox).
@@ -27,7 +27,7 @@ kubectl create configmap gardener-config --dry-run \
 find k8s/${CLUSTER}/ -type f -exec \
     sed -i \
       -e 's/{{GIT_COMMIT}}/'${GIT_COMMIT}'/g' \
-      -e 's/{{GCLOUD_PROJECT}}/'${PROJECT}'/g' \
+      -e 's/{{GCLOUD_PROJECT}}/'${PROJECT_ID}'/g' \
       -e 's/{{DATE_SKIP}}/'${DATE_SKIP}'/g' \
       -e 's/{{TASK_FILE_SKIP}}/'${TASK_FILE_SKIP}'/g' \
       {} \;
